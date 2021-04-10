176d1e24e591dffd635ea7603caf94931a45bed9
Evgesha4272 <76043094+Evgesha4272@users.noreply.github.com>
2021-04-10 11:47:29 +0300

    Update MyLogTest.php

--- tests/MyLogTest.php
+++ tests/MyLogTest.php
@@ -6,27 +6,11 @@ use Bezrukov\MyLog;
 class MyLogTest extends TestCase
 {
 
-    public function testWrite()
-    {
-
-        $this->assertEquals('', MyLog::write("Testing"));
-        $this->assertEquals('', MyLog::write(1234));
-        $this->assertEquals('', MyLog::write());
-
+     public function testLog(){
+        $this->expectOutputString("equation!");
+        MyLog::write();
+     }
+     public function testInstance() {
+        $this->assertInstanceOf(MyLog::class, MyLog::Instance());
     }
-
-    public function testMyLog()
-    {
-        $this->assertEquals('', MyLog::log("Bezrukov"));
-        $this->assertEquals('', MyLog::log(1546786));
-        $this->assertEquals('', MyLog::log(false));
-    }
-
-    public function testMyLogEx()
-    {
-        $this->expectException(TypeError::class);
-        $this->assertEquals('', MyLog::log());
-        $this->assertEquals('', MyLog::log(null));
-    }
-
 }
